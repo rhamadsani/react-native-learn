04505a277369b29a3532a8b1ce5c5e845342b503
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-11 14:20:24 +0700

    component

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,11 @@
 import { View, Alert ,Button} from "react-native";
+import Greet from '../components/Greet';
 
 export default function App() {
   return (
       <View style={{flex: 1, backgroundColor:"plum", padding: 60,}}>
-        <Button title="Alert" onPress={() => Alert.alert('Invalid Data Fetching')}/>
-        <Button title="Alert With Title" onPress={() => Alert.alert('Invalid Data Fetching', 'Detai Data Fail')}/>
-        <Button title="Alert With Optional Button" onPress={() => Alert.alert('Invalid Data Fetching', 'Detai Data Fail', [
-          {
-            text: 'Cancel', 
-            onPress : () => {
-              console.log("Cancel Pressed")
-            }
-          },
-          {
-            text: 'Oke', 
-            onPress : () => {
-              console.log("Oke Pressed")
-            }
-          }
-        ])}/>
+        <Greet name="Rhamad Nursani Sidik"></Greet>
+        <Greet name="Mudzaffar Bilal Sidik"></Greet>
       </View>
   )
 }
