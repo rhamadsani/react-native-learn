12309bbd911dbab2a9d84fa693b3dc0900d82b0c
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-11 14:14:27 +0700

    alert

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,24 @@
-import { View, ActivityIndicator } from "react-native";
+import { View, Alert ,Button} from "react-native";
 
 export default function App() {
   return (
       <View style={{flex: 1, backgroundColor:"plum", padding: 60,}}>
-        {/* size, color */}
-        <ActivityIndicator/>
-        <ActivityIndicator size="large"/>
-        <ActivityIndicator size="large" color="black"/>
-        <ActivityIndicator size="large" color="white" animating={false}/>
+        <Button title="Alert" onPress={() => Alert.alert('Invalid Data Fetching')}/>
+        <Button title="Alert With Title" onPress={() => Alert.alert('Invalid Data Fetching', 'Detai Data Fail')}/>
+        <Button title="Alert With Optional Button" onPress={() => Alert.alert('Invalid Data Fetching', 'Detai Data Fail', [
+          {
+            text: 'Cancel', 
+            onPress : () => {
+              console.log("Cancel Pressed")
+            }
+          },
+          {
+            text: 'Oke', 
+            onPress : () => {
+              console.log("Oke Pressed")
+            }
+          }
+        ])}/>
       </View>
   )
 }
