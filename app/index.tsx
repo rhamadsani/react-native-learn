a4c3a73f60136e77eddcdf3bc99881593f0072da
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-11 13:15:00 +0700

    view learn

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,23 @@
-import { View, Image, Text, ImageBackground} from "react-native";
+import { View, Image, Text, ScrollView} from "react-native";
 const logoImage = require('./../assets/images/adaptive-icon.png')
 
 export default function App() {
   return (
-    <View style={{flex: 1, backgroundColor:"plum", padding: 10,}}>
-      {/* <Image source={logoImage} style={{width: 300, height:300}}/> */}
-      {/* <Image source={{uri: 'https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124'}} style={{width: 300, height:300}}/> */}
-      <ImageBackground source={logoImage} style={{flex: 1,}}>
-        <Text>Image Text</Text>
-      </ImageBackground>
-    </View>
+      <View style={{flex: 1, backgroundColor:"plum", padding: 60,}}>
+        <ScrollView>
+          <Image source={logoImage} style={{width: 300, height:300}}/>
+          {/* <Image source={{uri: 'https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124'}} style={{width: 300, height:300}}/> */}
+          {/* <ImageBackground source={logoImage} style={{flex: 1,}}>
+            <Text>Image Text</Text>
+          </ImageBackground> */}
+          <Text>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae sint ut numquam, veniam aut quo dolorem placeat dicta ipsam nobis! Molestiae officia animi accusantium. Sit dolorem odit voluptatem nam a?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nam quo neque similique vel! Consequatur illo dolorum dolore nisi placeat aut repellendus accusantium laboriosam, quisquam minima, tempore, nesciunt possimus in.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptate quisquam numquam iure asperiores officiis consectetur omnis odio quo incidunt, ratione quia molestiae ducimus laudantium odit? Vero aspernatur quis enim.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est possimus, veritatis dolorum vel asperiores esse voluptatum consectetur culpa, deleniti, tenetur architecto ex ab nobis ipsum velit quia optio voluptas eligendi.
+          </Text>
+          <Image source={logoImage} style={{width: 300, height:300}}/>
+        </ScrollView>
+      </View>
   )
 }
