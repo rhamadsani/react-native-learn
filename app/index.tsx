ce6c1496fe5cdc9702befd2fb6a332827c62ced8
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-11 14:07:32 +0700

    ActivityIndicator

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
-import { View, StatusBar } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 
 export default function App() {
   return (
       <View style={{flex: 1, backgroundColor:"plum", padding: 60,}}>
-        {/* barStyle=> default, dark-content, light-content | hidden ->hide status bar */}
-        <StatusBar backgroundColor="plum" barStyle="light-content"/>
+        {/* size, color */}
+        <ActivityIndicator/>
+        <ActivityIndicator size="large"/>
+        <ActivityIndicator size="large" color="black"/>
+        <ActivityIndicator size="large" color="white" animating={false}/>
       </View>
   )
 }
