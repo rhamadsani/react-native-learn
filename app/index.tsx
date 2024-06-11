c5525f9c35159018cbee8415effac3e8e628fab7
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-11 14:42:05 +0700

    update for box model

--- app/index.tsx
+++ app/index.tsx
@@ -16,7 +16,14 @@ const styles = StyleSheet.create({
   box: {
     width: 100, 
     height: 100, 
-    padding: 10,
+    // padding: 10,
+    paddingHorizontal: 10, 
+    paddingVertical: 20, 
+    marginVertical: 10, 
+    borderWidth: 2, 
+    borderColor: '#fff', 
+    borderRadius: 20
+
   },
   lighblueBg: {
     backgroundColor: "lightblue", 
