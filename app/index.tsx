b4dcf4034414a38afdd3205ce20bbb81724cfc37
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-11 14:56:19 +0700

    shadow & elevation

--- app/index.tsx
+++ app/index.tsx
@@ -4,24 +4,24 @@ export default function App() {
   return (
       <View style={styles.container}>
         <View>
-          <Text style={[styles.lighblueBg, styles.box]}> LightBlue Box</Text>
-          <Text style={[styles.lightgreenBg, styles.box]}> LightGreen Box</Text>
+          <Text style={[styles.lighblueBg, styles.box, styles.boxShadow]}> LightBlue Box</Text>
+          <Text style={[styles.lightgreenBg, styles.box, styles.androidShadow]}> LightGreen Box</Text>
         </View>
       </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor:"plum", padding: 60},
+  container: {flex: 1, backgroundColor:"white", padding: 60},
   box: {
-    width: 100, 
-    height: 100, 
+    width: 250, 
+    height: 250, 
     // padding: 10,
     paddingHorizontal: 10, 
     paddingVertical: 20, 
     marginVertical: 10, 
     borderWidth: 2, 
-    borderColor: '#fff', 
+    borderColor: '#C7C8CC', 
     borderRadius: 20
 
   },
@@ -30,5 +30,17 @@ const styles = StyleSheet.create({
   },
   lightgreenBg: {
     backgroundColor: "lightgreen", 
+  }, 
+  boxShadow: {
+    shadowColor: "blue", 
+    shadowOffset: {
+      width: 6, 
+      height: 6,
+    }, 
+    shadowOpacity: 0.6,
+    shadowRadius: 4
+  },
+  androidShadow: {
+    elevation: 10,
   }
 })
