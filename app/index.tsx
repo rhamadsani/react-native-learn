a3e4bf07224eb73fd3e9291c046ba07ab493f624
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-11 14:04:29 +0700

    status bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,10 @@
-import { View, ScrollView, Button, Modal, Text} from "react-native";
-import { useState } from "react";
-
-const logoImage = require('./../assets/images/adaptive-icon.png')
+import { View, StatusBar } from "react-native";
 
 export default function App() {
-  const [isModalVisible, setModalVisible] = useState(false)
   return (
       <View style={{flex: 1, backgroundColor:"plum", padding: 60,}}>
-        <ScrollView>
-          <Button title="press" onPress={() => setModalVisible(true)} color="#e10102"/>
-          {/* animationType => fade, slide |presentationStyle => formSheet, pageSheet  */}
-          <Modal presentationStyle="pageSheet" animationType="slide" visible={isModalVisible} onRequestClose={() => setModalVisible(false)}>
-               <View style={{flex: 1, backgroundColor:"lightblue", padding: 60,}}>
-                  <Text>Modal Content</Text>
-                  <Button onPress={() => setModalVisible(false)} title="Close" color="red"/>
-               </View>
-          </Modal>
-        </ScrollView>
+        {/* barStyle=> default, dark-content, light-content | hidden ->hide status bar */}
+        <StatusBar backgroundColor="plum" barStyle="light-content"/>
       </View>
   )
 }
