10c61dd61c9765035a706dfbbc707c30306ac71b
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-12 09:22:36 +0700

    code setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,60 +1,23 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import Box from "../components/Box";
 
 export default function App() {
   return (
       <View style={styles.container}>
-        <View style={styles.darkMode}>
-          {/* inheritance work on Text -> Text not View -> Text */}
-            <Text style={styles.darkModeText}>Halo, <Text style={styles.boldText}>Rhamad Nursani Sidik</Text></Text>
-        </View>
-        <View>
-          <Text style={[styles.lighblueBg, styles.box, styles.boxShadow]}> LightBlue Box</Text>
-          <Text style={[styles.lightgreenBg, styles.box, styles.androidShadow]}> LightGreen Box</Text>
-        </View>
+        <Box style={{backgroundColor: "#8e9b00"}}>Box 1</Box>
+        <Box style={{backgroundColor: "#2196f3"}}>Box 2</Box>
+        <Box style={{backgroundColor: "#ff5722"}}>Box 3</Box>
+        <Box style={{backgroundColor: "#ffeb3b"}}>Box 4</Box>
+        <Box style={{backgroundColor: "#e91e63"}}>Box 5</Box>
+        <Box style={{backgroundColor: "#4caf50"}}>Box 6</Box>
       </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor:"white", padding: 60},
-  darkMode : {
-    backgroundColor: "black", 
-    padding: 10,
+  container: {
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red"
   },
-  darkModeText: {
-    color: "white",
-  },
-  boldText : {
-    fontWeight: "bold"
-  }, 
-  box: {
-    width: 250, 
-    height: 250, 
-    // padding: 10,
-    paddingHorizontal: 10, 
-    paddingVertical: 20, 
-    marginVertical: 10, 
-    borderWidth: 2, 
-    borderColor: '#C7C8CC', 
-    borderRadius: 20
-
-  },
-  lighblueBg: {
-    backgroundColor: "lightblue", 
-  },
-  lightgreenBg: {
-    backgroundColor: "lightgreen", 
-  }, 
-  boxShadow: {
-    shadowColor: "blue", 
-    shadowOffset: {
-      width: 6, 
-      height: 6,
-    }, 
-    shadowOpacity: 0.6,
-    shadowRadius: 4
-  },
-  androidShadow: {
-    elevation: 10,
-  }
 })
