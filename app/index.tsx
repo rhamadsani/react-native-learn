4a495d885e4be8d2c5393d715e5d61e8372ec7e9
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-12 09:40:59 +0700

    flex

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,12 @@ import Box from "../components/Box";
 
 export default function App() {
   return (
+    // <View style={{backgroundColor: "plum", flex: 1,}}>
+
+    // </View>
       <View style={styles.container}>
-        <Box style={{backgroundColor: "#8e9b00"}}>Box 1</Box>
-        <Box style={{backgroundColor: "#2196f3"}}>Box 2</Box>
+        <Box style={{backgroundColor: "#8e9b00", flex: 1}}>Box 1</Box>
+        <Box style={{backgroundColor: "#2196f3" ,flex: 3}}>Box 2</Box>
         <Box style={{backgroundColor: "#ff5722"}}>Box 3</Box>
         <Box style={{backgroundColor: "#ffeb3b"}}>Box 4</Box>
         <Box style={{backgroundColor: "#e91e63"}}>Box 5</Box>
@@ -16,6 +19,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red"
