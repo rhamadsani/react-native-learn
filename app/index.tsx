a0eb16e3b649413b721e7c57971b03bec9abfb22
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-12 09:46:32 +0700

    flex direction

--- app/index.tsx
+++ app/index.tsx
@@ -7,12 +7,12 @@ export default function App() {
 
     // </View>
       <View style={styles.container}>
-        <Box style={{backgroundColor: "#8e9b00", flex: 1}}>Box 1</Box>
-        <Box style={{backgroundColor: "#2196f3" ,flex: 3}}>Box 2</Box>
+        <Box style={{backgroundColor: "#8e9b00"}}>Box 1</Box>
+        <Box style={{backgroundColor: "#2196f3"}}>Box 2</Box>
         <Box style={{backgroundColor: "#ff5722"}}>Box 3</Box>
-        <Box style={{backgroundColor: "#ffeb3b"}}>Box 4</Box>
+        {/* <Box style={{backgroundColor: "#ffeb3b"}}>Box 4</Box>
         <Box style={{backgroundColor: "#e91e63"}}>Box 5</Box>
-        <Box style={{backgroundColor: "#4caf50"}}>Box 6</Box>
+        <Box style={{backgroundColor: "#4caf50"}}>Box 6</Box> */}
       </View>
   )
 }
@@ -20,6 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column-reverse", //column, column-reverse, row, row-reverse
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red"
