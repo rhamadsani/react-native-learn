8a8814dcaf2782a14ba2609ae932f3855c1f50b7
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-13 11:11:31 +0700

    align items

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function App() {
 
     // </View>
       <View style={styles.container}>
-        <Box style={{backgroundColor: "#8e9b00"}}>Box 1</Box>
+        <Box style={{backgroundColor: "#8e9b00", paddingVertical: 100}}>Box 1</Box>
         <Box style={{backgroundColor: "#2196f3"}}>Box 2</Box>
         <Box style={{backgroundColor: "#ff5722"}}>Box 3</Box>
         {/* <Box style={{backgroundColor: "#ffeb3b"}}>Box 4</Box>
@@ -20,8 +20,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-evenly", //flext-start, flext-end, center, space-between, space-around, space-evenly
+    // justifyContent: "space-evenly", //flext-start, flext-end, center, space-between, space-around, space-evenly
     // flexDirection: "column-reverse", //column, column-reverse, row, row-reverse
+    flexDirection: "row",
+    alignItems: "baseline", //stretch, flex-start, flex-end, baseline
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red" 
