f2a1e4415e0636c123f3863c707f3d584decf809
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-06-13 11:29:21 +0700

    flex wrap

--- app/index.tsx
+++ app/index.tsx
@@ -7,10 +7,10 @@ export default function App() {
 
     // </View>
       <View style={styles.container}>
-        <Box style={{backgroundColor: "#8e9b00", alignSelf: 'flex-start'}}>Box 1</Box>
-        <Box style={{backgroundColor: "#2196f3", alignSelf: 'flex-end'}}>Box 2</Box>
-        <Box style={{backgroundColor: "#ff5722", alignSelf: 'center'}}>Box 3</Box>
-        <Box style={{backgroundColor: "#ffeb3b",  alignSelf: 'auto'}}>Box 4</Box>
+        <Box style={{backgroundColor: "#8e9b00"}}>Box 1</Box>
+        <Box style={{backgroundColor: "#2196f3"}}>Box 2</Box>
+        <Box style={{backgroundColor: "#ff5722"}}>Box 3</Box>
+        <Box style={{backgroundColor: "#ffeb3b"}}>Box 4</Box>
         <Box style={{backgroundColor: "#e91e63"}}>Box 5</Box>
         <Box style={{backgroundColor: "#4caf50"}}>Box 6</Box>
       </View>
@@ -20,12 +20,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: "space-evenly", //flext-start, flext-end, center, space-between, space-around, space-evenly
-    // flexDirection: "column-reverse", //column, column-reverse, row, row-reverse
-    // flexDirection: "row",
-    // alignItems: "baseline", //stretch(default value), flex-start, flex-end, baseline
-    alignItems: "flex-end",
+    flexDirection: "row",
+    flexWrap: "wrap",
     marginTop: 64,
+    // height:300,
     borderWidth: 6,
     borderColor: "red" 
   },
